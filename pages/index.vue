cbe8370afb1edd06fd33cc4c4b714117a5cc1e26
Eduardo <eduardo@sting.net>
2021-02-02 11:06:44 -0400

    configured data fetching from contentul

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <section v-for="(procedure) in procedures" :key="procedure.sys.id">
       <h1>{{ procedure.fields.title }}</h1>
-      <p>{{ procedure.fields.description }}</p>
+      <!-- <p>{{ procedure.fields.description }}</p> -->
     </section>
     <Logo />
   </div>
@@ -14,11 +14,23 @@ import { createClient } from "~/plugins/contentful";
 const client = createClient();
 
 export default {
-  async asyncData({env}){    
+  data: () => ({
+
+  }),
+  async asyncData({env}){
     try{
-      const data = await client.getEntries({'content_type': env.CTF_CONTENT_TYPE_ID})
+      const data = await client.getEntries();
       console.log(data)
-      return { procedures: data.items }
+
+      const procedures = data.items.filter(item => item.sys.contentType.sys.id === env.CTF_PROCEDURE_TYPE)
+      const bodyAreas = data.items.filter(item => item.sys.contentType.sys.id === env.CTF_BODY_AREA_TYPE)
+      const categories = data.items.filter(item => item.sys.contentType.sys.id === env.CTF_CATEGORY_TYPE)
+
+      console.log(procedures)
+      console.log(bodyAreas)
+      console.log(categories)
+
+      return { procedures, bodyAreas, categories }
     } catch (e) {
       console.error(e)
     }
